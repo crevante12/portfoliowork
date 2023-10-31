33e292139e50661e85934697a2828432086e50b7
Crevante Holt <crevante.holt12@gmail.com>
2023-10-31 11:43:36 -0500

    server updated to run

--- server.js
+++ server.js
@@ -5,7 +5,10 @@ const morgan = require('morgan')
 const mongoose = require('mongoose')
 const {expressjwt} = require('express-jwt')
 const cors = require('cors') 
+const path = require("path")
 
+
+app.use(express.static(path.join(__dirname, "client", "build")))
 app.use(express.json())
 app.use(morgan('dev'))
 app.use(cors())
@@ -26,6 +29,9 @@ app.use((err, req, res, next) => {
   }
   return res.send({errMsg: err.message})
 })
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+});
 
 app.listen(6000, () => {
   console.log(`Server is running on local port 6000`)
